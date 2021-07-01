f40ba7d8fa8aabbc1aed9e0bc9786b047555ec4b
Ruhul Amin <43258231+ruhulaminrul@users.noreply.github.com>
2021-07-01 21:10:07 +0600

    payment Table

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
