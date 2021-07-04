53a87608bb65e9b6e15218ec2281472f6783dbeb
Ruhul Amin <43258231+ruhulaminrul@users.noreply.github.com>
2021-07-04 21:56:02 +0600

    payment filter

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,50 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Billing;
 use App\Models\payment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Fees;
+use Carbon\Carbon;
 
 class TeacherController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
 
-        // $teachers =  Auth::user()->campus->teachers();
-        $teachers = Auth::user()->billings;
-        return $teachers;
-        $totalMonth = 0;
+        $type = $request->type;
+        $teachers =  Auth::user()->campus->teachers();
+        if ($request->month) {
 
-        // if($request -> type == 'monthly'){
-        //     foreach ($teachers as $teacher) {
-        //         $fees = Fees::find(1);
-        //         if (!is_null($teacher->payments)) {
-        //             $months = 0;
-        //             foreach($teacher->payments as $payment){
-        //                 $months ++;
-        //             }
-        //             $teacher -> months = $months;
-        //             $teacher -> totalAmount = $fees -> monthly * $months;
-        //         }
-        //     }
-        //     return view('Admin.Teacher.index', compact('teachers'));
-        // }
+            $month =   Carbon::createFromFormat('Y-m', $request->month)->format('m');
+            $year  =  Carbon::createFromFormat('Y-m', $request->month)->format('Y');
+            // return compact('month','year');
 
-        if($request -> type == 'monthly'){
-            foreach ($teachers as $teacher) {
-                $fees = Fees::find(1);
-                if (!is_null($teacher->payments)) {
-                    $months = 0;
-                    foreach($teacher->payments as $payment){
-                        $months ++;
+
+            if ($request->type == 'monthly') {
+
+                $billings = Billing::where('type', 'monthly')->whereMonth('created_at', $month)->get();
+
+
+           
+
+                foreach ($teachers  as $teacher) {
+                    $ispay =  payment::where('user_id', $teacher->id)->where('year', $year)->where('month', $month)->first();
+                    if (!is_null($ispay)) {
+                        $teacher->ispay = 'Paid';
+                    } else {
+                        $teacher->ispay = 'Unpaid';
                     }
-                    $teacher -> months = $months;
-                    $teacher -> totalAmount = $fees -> monthly * $months;
                 }
+            } else if ($request->type == 'others') {
+                $billings = Billing::where('type', 'others')->whereMonth('created_at', $month)->get();
+            } else {
+                $billings = Billing::whereMonth('created_at', $month)->get();
             }
-            return view('Admin.Teacher.index', compact('teachers'));
+
+            return view('Admin.Teacher.index', compact('billings', 'type','teachers'));
+        } else {
         }
 
 
+
+        $month =   Carbon::createFromFormat('Y-m', $request->month)->format('m');
+        $year  =  Carbon::createFromFormat('Y-m', $request->month)->format('Y');
+        return compact('month', 'year');
+
+        $teachers =  Auth::user()->campus->teachers();
+
+
+
+
+
+        return view('Admin.Teacher.index', compact('teachers'));
     }
 }
--- app/Models/Billing.php
+++ app/Models/Billing.php
@@ -10,4 +10,9 @@ class Billing extends Model
     use HasFactory;
     protected $guarded = [];
 
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
 }
