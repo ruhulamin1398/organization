03f0ec5fca2d22832e312c28f8aba7d93691962f
aymansohag <aymansohag.contact@gmail.com>
2021-07-10 12:13:57 +0600

    Bec Campus Add

--- app/Models/Campus.php
+++ app/Models/Campus.php
@@ -5,13 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Campus extends Model 
+class Campus extends Model
 {
     use HasFactory;
     protected $guarded = [];
 
 
     public function teachers(){
-        return  User::where('campus_id',$this->id) ->where('id','>',4)->get();
+        return  User::where('campus_id',$this->id) ->where('id','>',5)->get();
     }
 }
